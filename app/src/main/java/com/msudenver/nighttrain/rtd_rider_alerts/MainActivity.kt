e1709df905f12d1dc21cdab6a0bbc41e5b43d8f6
Ahmadwali zadron <wzadron@msudenver.edu>
2020-01-30 17:07:01 -0700

    updates on recyclerview

--- app/src/main/java/com/msudenver/nighttrain/rtd_rider_alerts/MainActivity.kt
+++ app/src/main/java/com/msudenver/nighttrain/rtd_rider_alerts/MainActivity.kt
@@ -2,6 +2,9 @@ package com.msudenver.nighttrain.rtd_rider_alerts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
@@ -16,25 +19,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.textView)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        val adapter = TrainTimeAdapter(arrayOf<String>("home", "work", "school"))
+        recyclerView.adapter = adapter
 
         // https://developer.android.com/training/volley/simple.html
         // https://tutorial.eyehunts.com/android/volley-android-example-json-parsing-kotlin/
 
-        val requestQueue = Volley.newRequestQueue(this)
+        /*val requestQueue = Volley.newRequestQueue(this)
 
         val alertsRequest = GsonRequest(url, RTDAlertData::class.java, null, Response.Listener {
             response -> textView.text =response.toString()
         },
             Response.ErrorListener { error -> textView.text = error.toString() }
         )
-        /*val stringRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener { response ->
+        val stringRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener { response ->
                 textView.text = response.toString().substring(0, 500)
             },
             Response.ErrorListener { error -> textView.text = "That didn't work!" }
         )
-        stringRequest.tag = tag*/
-        requestQueue.add(alertsRequest)
+        stringRequest.tag = tag
+        requestQueue.add(alertsRequest)*/
 
     }
 
---
+++ app/src/main/java/com/msudenver/nighttrain/rtd_rider_alerts/TrainTimeAdapter.kt
@@ -0,0 +1,42 @@
+package com.msudenver.nighttrain.rtd_rider_alerts
+
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class TrainTimeAdapter (private val myDataset: Array<String>) :
+        RecyclerView.Adapter<TrainTimeAdapter.MyViewHolder>() {
+    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bindItems(user : String)
+        {
+            val textDestination = view.findViewById<TextView>(R.id.textDestination)
+            val textLine = view.findViewById<TextView>(R.id.textLine)
+            val textTime = view.findViewById<TextView>(R.id.textTime)
+
+            textDestination.text = user
+            textTime.text = "1010pm"
+            textLine.text = "F"
+            textLine.setBackgroundColor(Color.RED)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): TrainTimeAdapter.MyViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.train_time_card, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+       return myDataset.size
+    }
+
+    override fun onBindViewHolder(holder: TrainTimeAdapter.MyViewHolder, p1: Int) {
+        holder.bindItems((myDataset[p1]))
+    }
+
+}
