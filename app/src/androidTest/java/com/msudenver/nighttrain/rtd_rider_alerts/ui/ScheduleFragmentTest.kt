bb2189157c57e12e429bb13a3dd5119532ed9418
Steven Wancewicz <swancewi@msudenver.edu>
2020-04-10 17:09:32 -0600

    fixed warnings, added ui test

--- app/src/androidTest/java/com/msudenver/nighttrain/rtd_rider_alerts/ui/ScheduleFragmentTest.kt
+++ app/src/androidTest/java/com/msudenver/nighttrain/rtd_rider_alerts/ui/ScheduleFragmentTest.kt
@@ -2,10 +2,14 @@ package com.msudenver.nighttrain.rtd_rider_alerts.ui
 
 import android.content.Context
 import android.widget.Spinner
+import androidx.lifecycle.ViewModelProvider
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
@@ -25,13 +29,12 @@ class ScheduleFragmentTest {
 
     @Test
     fun testRotate() {
-        activityRule.activity.theFragment.viewModel.stationNames.postValue(listOf("10th & Osagio", station))
-        activityRule.activity.theFragment.viewModel.stationSelected.postValue(station)
+        val viewModel = ViewModelProvider(activityRule.activity).get(TrainScheduleViewModel::class.java)
         val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         device.waitForIdle()
         Thread.sleep(3000)
-        activityRule.activity.theFragment.viewModel.stationNames.postValue(listOf("10th & Osagio", station))
-        activityRule.activity.theFragment.viewModel.stationSelected.postValue(station)
+        viewModel.stationNames.postValue(listOf("10th & Osagio", station))
+        viewModel.stationSelected.postValue(station)
         device.setOrientationRight()
 
         Thread.sleep(3000)
@@ -60,5 +63,24 @@ class ScheduleFragmentTest {
         scheduleFragment.testableContext = ApplicationProvider.getApplicationContext()
         scheduleFragment.updateSpinnerList(stationList)
         Truth.assertThat(scheduleFragment.stationsSpinner?.adapter?.getItem(1)).isEqualTo(station)
+    }
+
+    @Test
+    fun testViewFavoriteStations() {
+
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device.waitForIdle()
+        val viewModel = ViewModelProvider(activityRule.activity).get(InterfaceViewModel::class.java)
+        onView(withId(R.id.add_station)).perform(click())
+        device.waitForIdle()
+
+        Truth.assertThat(viewModel.showStations.value).isTrue()
+
+        //click other button
+        onView(withId(R.id.back_schedule_button)).perform(click())
+        device.waitForIdle()
+
+        Truth.assertThat(viewModel.showStations.value).isFalse()
+        //confirm fragment is Schedule fragment
     }
 }
