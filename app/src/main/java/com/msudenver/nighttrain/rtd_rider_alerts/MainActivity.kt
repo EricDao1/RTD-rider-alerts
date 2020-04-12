61cbad8ecb863a5d4ece7748e608d08759940c16
Steven Wancewicz <swancewi@msudenver.edu>
2020-04-11 19:52:58 -0600

    seeing if removing test coverage fixes circleci build fail

--- app/src/main/java/com/msudenver/nighttrain/rtd_rider_alerts/MainActivity.kt
+++ app/src/main/java/com/msudenver/nighttrain/rtd_rider_alerts/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.annotation.VisibleForTesting
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import com.msudenver.nighttrain.rtd_rider_alerts.ui.FavoriteStationFragment
 import com.msudenver.nighttrain.rtd_rider_alerts.ui.InterfaceViewModel
 import com.msudenver.nighttrain.rtd_rider_alerts.ui.ScheduleFragment
