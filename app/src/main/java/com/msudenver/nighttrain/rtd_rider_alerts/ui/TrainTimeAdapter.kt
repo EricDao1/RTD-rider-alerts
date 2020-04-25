fe798a3422d1005eec940adc7e1c01c7d08cb3e7
Steven Wancewicz <swancewi@msudenver.edu>
2020-04-24 20:12:23 -0600

    refactored traintime adapter for testing, added tests

--- app/src/main/java/com/msudenver/nighttrain/rtd_rider_alerts/ui/TrainTimeAdapter.kt
+++ app/src/main/java/com/msudenver/nighttrain/rtd_rider_alerts/ui/TrainTimeAdapter.kt
@@ -1,12 +1,14 @@
 package com.msudenver.nighttrain.rtd_rider_alerts.ui
 
 import android.graphics.Color
+import android.view.Display
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.VisibleForTesting
 import androidx.recyclerview.widget.RecyclerView
+import com.msudenver.nighttrain.rtd_rider_alerts.DisplayUtils
 import com.msudenver.nighttrain.rtd_rider_alerts.R
 import com.msudenver.nighttrain.rtd_rider_alerts.db.ScheduledTrain
 import java.text.SimpleDateFormat
@@ -23,18 +25,10 @@ class TrainTimeAdapter (
             val textDestination = view.findViewById<TextView>(R.id.textDestination)
             val textLine = view.findViewById<TextView>(R.id.textLine)
             val textTime = view.findViewById<TextView>(R.id.textTime)
-            val formatter = SimpleDateFormat("hh:mm aa")
-            var mtDate = Calendar.getInstance()
-            mtDate.timeInMillis = scheduledTrain.time.time + 7*60*60*1000 //7 hours in milliseconds :(
 
             textDestination.text = scheduledTrain.tripHeader
-            if(scheduledTrain.cancelledAlert > 0) {
-                textTime.text = "CANCELLED"
-                textTime.setTextColor(Color.RED)
-            } else {
-                textTime.text = formatter.format(mtDate.time)
-                textTime.setTextColor(Color.parseColor(String.format("#424242")))
-            }
+            textTime.text = DisplayUtils.getTrainTime(scheduledTrain)
+            textTime.setTextColor(DisplayUtils.getTimeColor(scheduledTrain))
             textLine.text = scheduledTrain.trainName
             textLine.setBackgroundColor(Color.parseColor(String.format("#%06X", scheduledTrain.routeColor)))
             textLine.setTextColor(Color.parseColor(String.format("#%06X",scheduledTrain.routeTextColor)))
@@ -42,7 +36,6 @@ class TrainTimeAdapter (
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): MyViewHolder {
-
         val view = LayoutInflater.from(parent.context).inflate(R.layout.train_time_card, parent, false)
         return MyViewHolder(
             view
