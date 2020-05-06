d5d60eab3d10e08efc1b6961bc262bb5754945e8
Steven Wancewicz <swancewi@msudenver.edu>
2020-05-05 18:57:25 -0600

    test for recycler view fragment click

--- app/src/androidTest/java/com/msudenver/nighttrain/rtd_rider_alerts/ui/FavoriteStationFragmentTest.kt
+++ app/src/androidTest/java/com/msudenver/nighttrain/rtd_rider_alerts/ui/FavoriteStationFragmentTest.kt
@@ -1,10 +1,14 @@
 package com.msudenver.nighttrain.rtd_rider_alerts.ui
 
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.test.annotation.UiThreadTest
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
@@ -12,8 +16,10 @@ import androidx.test.uiautomator.UiDevice
 import com.google.common.truth.Truth
 import com.msudenver.nighttrain.rtd_rider_alerts.MainActivity
 import com.msudenver.nighttrain.rtd_rider_alerts.R
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
+
 
 class FavoriteStationFragmentTest {
 
@@ -66,5 +72,44 @@ class FavoriteStationFragmentTest {
         onView(withId(R.id.add_station)).perform(click())
         device.waitForIdle()
         onView(withId(R.id.search_text)).perform(typeText("help"))
+    }
+
+    //https://github.com/android/android-test/blob/7e834ce37faf52f2a65a73b0a6d83ab148707cbb/testapps/ui_testapp/javatests/androidx/test/ui/app/AdapterViewTest.java
+    @Test
+    fun clickOnAdapterButton() {
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device.waitForIdle()
+        onView(withId(R.id.add_station)).perform(click())
+        device.waitForIdle()
+        onView(withId(R.id.search_text)).perform(typeText("Osage"))
+        device.waitForIdle()
+        onView(withId(R.id.fav_station_recycler))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<StationPickerAdapter.ViewHolder>(0,MyViewAction.clickChildViewWithId(
+                R.id.favoriteSwitch)))
+            //.atPosition(0)
+            //.onChildView(withId(R.id.favoriteSwitch))
+            //.perform(click())
+        device.waitForIdle()
+
+    }
+
+    //https://stackoverflow.com/questions/28476507/using-espresso-to-click-view-inside-recyclerview-item
+    object MyViewAction {
+        fun clickChildViewWithId(id: Int): ViewAction {
+            return object : ViewAction {
+                override fun getConstraints(): Matcher<View>? {
+                    return null
+                }
+
+                override fun getDescription(): String {
+                    return "Click on a child view with specified id."
+                }
+
+                override fun perform(uiController: UiController?, view: View) {
+                    val v: View = view.findViewById(id)
+                    v.performClick()
+                }
+            }
+        }
     }
 }
